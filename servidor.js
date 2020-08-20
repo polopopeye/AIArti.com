d562a2fb08bdb6a40bef929341e3052fa39e143b
1d3a portatil - Kenneth Suarez <kenneth.suarez@1d3a.com>
2020-08-20 15:26:23 +0200

    ultima subida para test a entorno
    netlify prueba

--- servidor.js
+++ servidor.js
@@ -11,7 +11,7 @@ var http = require('http'),
 
 
 var app=express();
-app.use(express.json({ limit: 1000000000 })); //1K MB
+app.use(express.json({ limit: 1000000000 })); //1K MB //actualmente 1GB
 app.use(express.static('assets'));
 app.set('views', 'views');
 app.engine('ejs', engine);
@@ -23,261 +23,220 @@ limit: 1000000000,
   extended: true
 }));
 
-mongoose.connect('mongodb://localhost:27017/AIArtiDB', {useNewUrlParser: true,useUnifiedTopology: true});
-const db = mongoose.connection;
-const aiClassSchema = new mongoose.Schema({
-  id: Number,
-  status: String,
-  tittle: String,
-  label: String,
-  note: String,
-  json: String
-});
-
-const aiImportedData = new mongoose.Schema({
-  id: Number,
-  status: String,
-  tittle: String,
-  label: String,
-  note: String,
-  data: String,
-  jsonTittle: String
-});
-
-const aiClass = mongoose.model('SavedData', aiClassSchema,'SavedClassData');
-const aiData = mongoose.model('SavedData2', aiImportedData,'ArticleInputData');
-function aiClassToDB(tittle,label,json,note){
-
-  var aiClassResult = new aiClass({
-    id: Math.floor(Math.random() * 999999999999999999),
-    status: "new",
-    tittle: tittle,
-    note: note,
-    json: json,
-    label: label });
-  console.log(aiClassResult); // 'Silence'
-  // const
-  aiClassResult.save(function (err) {
-   if (err) return console.error(err);
-   console.log("Dato guardado correctamente manin");
- });
-}
-function aiDataToDB(tittle,label,note,data,jsonTittle){
-
-  var aiDataResult = new aiData({
-    id: Math.floor(Math.random() * 999999999999999999),
-    status: "new",
-    tittle: tittle,
-    label: label,
-    note: note,
-    data: data,
-    jsonTittle:jsonTittle
-     });
-  console.log(aiDataResult); // 'Silence'
-  // const
-  aiDataResult.save(function (err) {
-   if (err) return console.error(err);
-   console.log("Dato guardado correctamente manin");
- });
-}
-
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-console.log("conectado que mas hay que hacer...");
-
-
-
-});
-
-
-
 
 
 //
-
-
-// END CONFIG
-
-// INI MARKOV MODEL text
-
-// var result =
-// setInterval(function(){
-// console.log();
-// },10);
-// console.log("result");
-// console.log();
-//
-// console.log(markov.generate(markovoptions).string);
-// console.log("result");
-
-// app.get("/",function(req,res){
-// res.render('ejs/index.ejs');
+// mongoose.connect('mongodb://localhost:27017/AIArtiDB', {useNewUrlParser: true,useUnifiedTopology: true});
+// const db = mongoose.connection;
+// const aiClassSchema = new mongoose.Schema({
+//   id: Number,
+//   status: String,
+//   tittle: String,
+//   label: String,
+//   note: String,
+//   json: String
 // });
-
-
-
-app.get("/MotorEngineStart",function(req,res){
-res.render('ejs/ArtiMotor/MotorEngineStart.ejs');
-});
-app.get("/TrytoClassify",function(req,res){
-res.render('ejs/ArtiMotor/TrytoClassify.ejs');
-});
-app.get("/AddData",function(req,res){
-  var actualApi=Math.floor(Date.now() / 1000)*6;//9581855202
-if (req.query.method!=undefined&&req.query.apiK!=undefined&&req.query.apiK==1) {
-
-if (req.query.method=="aiClassList") {
-  aiClass.find(function (err, aiClass) {
-  if (err) return console.error(err);
-  res.send(aiClass);
-})
-}
-if (req.query.method=="aiDataList") {
-  aiData.find(function (err, aiData) {
-  if (err) return console.error(err);
-  res.send(aiData);
-})
-}
-
-}else{
-  res.render('ejs/ArtiMotor/angryShark.ejs');
-}
-
-// if (=) {
-//   aiClassResult
+//
+// const aiImportedData = new mongoose.Schema({
+//   id: Number,
+//   status: String,
+//   tittle: String,
+//   label: String,
+//   note: String,
+//   data: String,
+//   jsonTittle: String
+// });
+//
+// const aiClass = mongoose.model('SavedData', aiClassSchema,'SavedClassData');
+// const aiData = mongoose.model('SavedData2', aiImportedData,'ArticleInputData');
+// function aiClassToDB(tittle,label,json,note){
+//
+//   var aiClassResult = new aiClass({
+//     id: Math.floor(Math.random() * 999999999999999999),
+//     status: "new",
+//     tittle: tittle,
+//     note: note,
+//     json: json,
+//     label: label });
+//   console.log(aiClassResult); // 'Silence'
+//   // const
+//   aiClassResult.save(function (err) {
+//    if (err) return console.error(err);
+//    console.log("Dato guardado correctamente manin");
+//  });
+// }
+// function aiDataToDB(tittle,label,note,data,jsonTittle){
+//
+//   var aiDataResult = new aiData({
+//     id: Math.floor(Math.random() * 999999999999999999),
+//     status: "new",
+//     tittle: tittle,
+//     label: label,
+//     note: note,
+//     data: data,
+//     jsonTittle:jsonTittle
+//      });
+//   console.log(aiDataResult); // 'Silence'
+//   // const
+//   aiDataResult.save(function (err) {
+//    if (err) return console.error(err);
+//    console.log("Dato guardado correctamente manin");
+//  });
 // }
 //
-// req.query.tagId
-// req.query.tagId
-
-console.log(req.query);
-});
-
-
-app.post("/AddData",function(req,res){
-  var actualApi=Math.floor(Date.now() / 1000)*6;//9581855202
-if (req.body.method!=undefined&&req.body.apiK!=undefined&&req.body.apiK==1) {
-
-if (req.body.method=="aiClassToDB") {
-  if (req.body.tittle&&
-    req.body.json&&
-    req.body.note&&
-    req.body.label) {
-      // aiClassToDB(tittle,label,json,note)
-    aiClassToDB(req.body.tittle,req.body.label,req.body.json,req.body.note);
-    res.render('ejs/ArtiMotor/dbUpdated.ejs');
-  }
-}
-if (req.body.method=="aiDataToDB") {
-  if (req.body.tittle&&
-    req.body.data&&
-    req.body.note&&
-    req.body.label) {
-      // aiClassToDB(tittle,label,json,note)
-    aiDataToDB(req.body.tittle,req.body.label,req.body.note,req.body.data,req.body.jsonTittle);
-    res.render('ejs/ArtiMotor/dbUpdated.ejs');
-  }
-}
-
-
-}else{
-  res.render('ejs/ArtiMotor/angryShark.ejs');
-}
-
-console.log(req.body);
-});
-// app.get("/statusMotor",function(req,res){
-// res.render('ejs/index.ejs');
-// });
-app.get("/scrapeArticle",function(req,res){
-  if (req.query.imported=="1") {
-    // res.render('ejs/ArtiMotor/articleScrapeImported.ejs');
-    var loaded=false;
-
-    aiData.find(function (err, aiData) {
-    if (err) return console.error(err);
-
-    var markov = new Markov(JSON.parse(aiData[0].data), { stateSize: 3})
-    markov.buildCorpus();
-     var markovoptions = {
-        maxTries: 100000, // Give up if I don't have a sentence after 20 tries (default is 10)
-       prng: Math.random // An external Pseudo Random Number Generator if you want to get seeded results
-     }
-var string;
-function newWord(){
-string=markov.generate(markovoptions);
-// if (string.score <= 700) {
-// if (string.score <= 300) {
-if (string.score <= 1) {
-// if (string.score <= 800) {
- // if ( string.string.length<200) {
-newWord();
-}else{
-  // res.send();
-  console.log(string);
-  loaded=true;
-}
-}
-newWord();
-// console.log(aiData[0].jsonTittle);
-
-// var markov2 = new Markov(JSON.parse(aiData[0].jsonTittle), { stateSize: 2})
-var markov2 = new Markov(JSON.parse(aiData[0].jsonTittle), { stateSize: 3})//maximo a no ser que se limite de otro lado
-markov2.buildCorpus();
- var markov2options = {
-    maxTries: 100000, // Give up if I don't have a sentence after 20 tries (default is 10)
-   prng: Math.random // An external Pseudo Random Number Generator if you want to get seeded results
- }
-var string2;
-function newWord2(){
-string2=markov2.generate(markov2options);
-// if (string2.score <= 700) {
-// if (string2.score <= 300) {
-if (string2.score>=10||loaded==false) {
-
-  newWord2();
-
-}else{
-    console.log(string2);
-    res.send("Titulo:"+string2.string+"<br>Articulo:<br>"+string.string);
-        // res.send("Titulo:"+string2.string);
-        console.log(string.score);
-        console.log(string2.score);
-
-}
-}
-newWord2();
-     // if (string.score>700) {
-     // }else{
-       // var string1=markov2.generate(markovoptions);
-       // res.send(string1.string);
-       // console.log(string1);
-     // }
-  });
-  }else{
-    res.render('ejs/ArtiMotor/articleScrape.ejs');
-  }
-});
-// app.get("/createArtiFromInput",function(req,res){
-//   const data = ['buenosdias','como estan','espero que muy bien','todo el texto tiene que tener',
-//   'mucho sentido porque si no no sirve de nah',
-//   'este es un texto de ejemplo',
-//   'bueno que vaya bien señor',
-//   'hola como',
-//   'hola como estas',
-//   'hola como estas tu',
-//   'espero que bien',
-//   'esta frase tiene mucho sentido',
-//   'estoy haciendo muchas pruebas',
-//   'espero que añadiendo estas pruebas de ejemplo sirva de algo',
-//   'el resultado tiene que ser esparanzador y prometedor',
-//   'bueno que vaya genial humano'];
+//
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+// console.log("conectado que mas hay que hacer...");
 //
 //
-//   res.send();
+//
+// });
+//
+//
+//
+//
+//
+//
+//
+// // END CONFIG
+//
+// // INI MARKOV MODEL text
+//
+//
+//
+//
+// app.get("/MotorEngineStart",function(req,res){
+// res.render('ejs/ArtiMotor/MotorEngineStart.ejs');
+// });
+// app.get("/TrytoClassify",function(req,res){
+// res.render('ejs/ArtiMotor/TrytoClassify.ejs');
+// });
+// app.get("/AddData",function(req,res){
+//   var actualApi=Math.floor(Date.now() / 1000)*6;//9581855202
+// if (req.query.method!=undefined&&req.query.apiK!=undefined&&req.query.apiK==1) {
+//
+// if (req.query.method=="aiClassList") {
+//   aiClass.find(function (err, aiClass) {
+//   if (err) return console.error(err);
+//   res.send(aiClass);
+// })
+// }
+// if (req.query.method=="aiDataList") {
+//   aiData.find(function (err, aiData) {
+//   if (err) return console.error(err);
+//   res.send(aiData);
+// })
+// }
+//
+// }else{
+//   res.render('ejs/ArtiMotor/angryShark.ejs');
+// }
+//
+//
+// console.log(req.query);
+// });
+//
+//
+// app.post("/AddData",function(req,res){
+//   var actualApi=Math.floor(Date.now() / 1000)*6;//9581855202
+// if (req.body.method!=undefined&&req.body.apiK!=undefined&&req.body.apiK==1) {
+//
+// if (req.body.method=="aiClassToDB") {
+//   if (req.body.tittle&&
+//     req.body.json&&
+//     req.body.note&&
+//     req.body.label) {
+//       // aiClassToDB(tittle,label,json,note)
+//     aiClassToDB(req.body.tittle,req.body.label,req.body.json,req.body.note);
+//     res.render('ejs/ArtiMotor/dbUpdated.ejs');
+//   }
+// }
+// if (req.body.method=="aiDataToDB") {
+//   if (req.body.tittle&&
+//     req.body.data&&
+//     req.body.note&&
+//     req.body.label) {
+//       // aiClassToDB(tittle,label,json,note)
+//     aiDataToDB(req.body.tittle,req.body.label,req.body.note,req.body.data,req.body.jsonTittle);
+//     res.render('ejs/ArtiMotor/dbUpdated.ejs');
+//   }
+// }
+//
+//
+// }else{
+//   res.render('ejs/ArtiMotor/angryShark.ejs');
+// }
+//
+// console.log(req.body);
+// });
+// // app.get("/statusMotor",function(req,res){
+// // res.render('ejs/index.ejs');
+// // });
+// app.get("/scrapeArticle",function(req,res){
+//   if (req.query.imported=="1") {
+//     // res.render('ejs/ArtiMotor/articleScrapeImported.ejs');
+//     var loaded=false;
+//
+//     aiData.find(function (err, aiData) {
+//     if (err) return console.error(err);
+//
+//     var markov = new Markov(JSON.parse(aiData[0].data), { stateSize: 3})
+//     markov.buildCorpus();
+//      var markovoptions = {
+//         maxTries: 100000, // Give up if I don't have a sentence after 20 tries (default is 10)
+//        prng: Math.random // An external Pseudo Random Number Generator if you want to get seeded results
+//      }
+// var string;
+// function newWord(){
+// string=markov.generate(markovoptions);
+//
+// if (string.score <= 1) {
+//
+// newWord();
+// }else{
+//   // res.send();
+//   console.log(string);
+//   loaded=true;
+// }
+// }
+// newWord();
+// // console.log(aiData[0].jsonTittle);
+//
+// // var markov2 = new Markov(JSON.parse(aiData[0].jsonTittle), { stateSize: 2})
+// var markov2 = new Markov(JSON.parse(aiData[0].jsonTittle), { stateSize: 2})//maximo a no ser que se limite de otro lado
+// markov2.buildCorpus();
+//  var markov2options = {
+//     maxTries: 100000, // Give up if I don't have a sentence after 20 tries (default is 10)
+//    prng: Math.random // An external Pseudo Random Number Generator if you want to get seeded results
+//  }
+// var string2;
+// function newWord2(){
+// string2=markov2.generate(markov2options);
+//
+// console.log("STRING GENERATED");
+// if (string2.score>=10||loaded==false) {
+// setTimeout(function(){
+//   newWord2();
+// },500);
+//
+// }else{
+//     console.log(string2);
+//     res.send("Titulo:"+string2.string+"<br>Articulo:<br>"+string.string);
+//         console.log(string.score);
+//         console.log(string2.score);
+//
+// }
+// }
+// newWord2();
+//
+//   });
+//   }else{
+//     res.render('ejs/ArtiMotor/articleScrape.ejs');
+//   }
 // });
 
+// FIN
 
 
 app.get("/login",function(req,res){
@@ -310,7 +269,9 @@ app.get("/administrator",function(req,res){
 app.get("/administrator.php",function(req,res){
   res.render('ejs/ArtiMotor/angryShark.ejs');
 });
-
+app.get("/",function(req,res){
+res.render('ejs/ArtiMotor/workInProgress.ejs');
+});
 app.listen(8968);
 
 setTimeout(lineaSeparatoria, 100);
